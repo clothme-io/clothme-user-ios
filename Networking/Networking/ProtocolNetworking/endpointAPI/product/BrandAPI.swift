22ae80425487f7220a3bf30742eacbd107a1ac27
clothme-io <clothme2019@outlook.com>
2020-06-22 12:10:35 -0400

    updated folders

---
+++ Networking/Networking/ProtocolNetworking/endpointAPI/product/BrandAPI.swift
@@ -0,0 +1,9 @@
+//
+//  BrandAPI.swift
+//  Networking
+//
+//  Created by MACPRO on 2020-06-22.
+//  Copyright © 2020 Paul Ikhane. All rights reserved.
+//
+
+import Foundation
---
+++ Networking/Networking/ProtocolNetworking/manager/product/CartNetworkManager.swift
@@ -0,0 +1,24 @@
+//
+//  CartNetworkManager.swift
+//  Networking
+//
+//  Created by MACPRO on 2020-06-22.
+//  Copyright © 2020 Paul Ikhane. All rights reserved.
+//
+
+import Foundation
+
+struct CartNetworkManager {
+    static let environment: NetworkEnvironment = .production
+    static let accountRouter = Router<AccountAPI>()
+    
+    fileprivate func handleNetworkResponse(_ response: HTTPURLResponse) -> Result<String, NetworkResponse> {
+        switch response.statusCode {
+        case 200...299: return .success("String")
+        case 401...500: return .failure(NetworkResponse.authenticationError)
+        case 501...599: return .failure(NetworkResponse.badRequest)
+        case 600: return .failure(NetworkResponse.outdated)
+        default: return .failure(NetworkResponse.failed)
+        }
+    }
+}
