9e43a73209b35f33c29126522dd71ee73d9f134c
clothme-io <clothme2019@outlook.com>
2020-06-22 11:21:48 -0400

    updated folders

--- User/User/infrastructure/account/endpoint/AccountEndpoints.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AccountEndpoint.swift
-//  User
-//
-//  Created by MACPRO on 2020-05-04.
-//  Copyright © 2020 Paul Ikhane. All rights reserved.
-//
-
-import Foundation
--- User/User/infrastructure/connection/endpoint/ConnectionEndpoints.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ConnectionEndpoints.swift
-//  User
-//
-//  Created by MACPRO on 2020-05-04.
-//  Copyright © 2020 Paul Ikhane. All rights reserved.
-//
-
-import Foundation
