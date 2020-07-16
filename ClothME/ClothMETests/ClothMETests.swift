fbd542042460e6e24354059cb08a9f248775eab4
clothme-io <clothme2019@outlook.com>
2020-07-15 22:22:57 -0400

    updated code folder

---
+++ ClothME/ClothMETests/ClothMETests.swift
@@ -0,0 +1,24 @@
+//
+//  ClothMETests.swift
+//  ClothMETests
+//
+//  Created by MACPRO on 2020-07-15.
+//  Copyright © 2020 Paul Ikhane. All rights reserved.
+//
+
+import XCTest
+@testable import Core
+
+class ClothMETests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+
+}
--- Core/CoreTests/CoreTests.swift
+++ Core/CoreTests/CoreTests.swift
@@ -1,31 +1,32 @@
 //
-//  UserTests.swift
-//  UserTests
+//  CoreTests.swift
+//  CoreTests
 //
-//  Created by MACPRO on 2020-04-20.
+//  Created by MACPRO on 2020-07-15.
 //  Copyright © 2020 Paul Ikhane. All rights reserved.
 //
 
 import XCTest
+@testable import Core
 
-class UserTests: XCTestCase {
+class CoreTests: XCTestCase {
 
-    override func setUpWithError() throws {
+    override func setUpWithError() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDownWithError() throws {
+    override func tearDownWithError() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
+    func testPerformanceExample() {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
