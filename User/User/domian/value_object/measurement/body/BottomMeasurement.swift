f033e685df4d52db697448bb67eee2bf63fe131a
clothme-io <clothme2019@outlook.com>
2020-07-20 17:22:36 -0400

    updated measurement

--- User/User/domian/value_object/measurement/body/BottomMeasurement.swift
+++ User/User/domian/value_object/measurement/body/BottomMeasurement.swift
@@ -7,15 +7,131 @@
 //
 
 import Foundation
+import Core
 
 struct BottomMeasurement : Equatable {
-    private var waist: MeasurementUnit
-    private var inseam: MeasurementUnit
-    private var croach: MeasurementUnit
-    private var hip: MeasurementUnit
-    private var waistToAnkle: MeasurementUnit
-    private var ankle: MeasurementUnit?
-    private var waistToKnee: MeasurementUnit?
-    private var waistToHip: MeasurementUnit?
-    private var highWaist: MeasurementUnit?
+    private var _waist: MeasurementUnit
+    private var _highWaist: MeasurementUnit
+    private var _hip: MeasurementUnit
+    private var _thigh: MeasurementUnit
+    private var _croach: MeasurementUnit
+    private var _inseamToKnee: MeasurementUnit
+    private var _inseamToHeel: MeasurementUnit
+    private var _waistToAnkle: MeasurementUnit
+    private var _waistToHeel: MeasurementUnit
+    private var _waistToKnee: MeasurementUnit
+    
+    private init(
+        waist: MeasurementUnit,
+        highWaist: MeasurementUnit,
+        hip: MeasurementUnit,
+        thigh: MeasurementUnit,
+        croach: MeasurementUnit,
+        inseamToKnee: MeasurementUnit,
+        inseamToHeel: MeasurementUnit,
+        waistToAnkle: MeasurementUnit,
+        waistToHeel: MeasurementUnit,
+        waistToKnee: MeasurementUnit
+    ) {
+        self._waist = waist
+        self._highWaist = highWaist
+        self._hip = hip
+        self._thigh = thigh
+        self._croach = croach
+        self._inseamToKnee = inseamToKnee
+        self._inseamToHeel = inseamToHeel
+        self._waistToAnkle = waistToAnkle
+        self._waistToHeel = waistToHeel
+        self._waistToKnee = waistToKnee
+    }
+    
+    static func create(
+        waist: MeasurementUnit,
+        highWaist: MeasurementUnit,
+        hip: MeasurementUnit,
+        thigh: MeasurementUnit,
+        croach: MeasurementUnit,
+        inseamToKnee: MeasurementUnit,
+        inseamToHeel: MeasurementUnit,
+        waistToAnkle: MeasurementUnit,
+        waistToHeel: MeasurementUnit,
+        waistToKnee: MeasurementUnit
+    ) -> ResultOption<BottomMeasurement, AppError> {
+        if (!validateNotNil(waist: waist, highWaist: highWaist, hip: hip, thigh: thigh, croach: croach, inseamToKnee: inseamToKnee, inseamToHeel: inseamToHeel, waistToAnkle: waistToAnkle, waistToHeel: waistToHeel, waistToKnee: waistToKnee)) {
+            return .error(AppError.nilValueNotAllowed)
+        }
+        return .ok(BottomMeasurement(waist: waist, highWaist: highWaist, hip: hip, thigh: thigh, croach: croach, inseamToKnee: inseamToKnee, inseamToHeel: inseamToHeel, waistToAnkle: waistToAnkle, waistToHeel: waistToHeel, waistToKnee: waistToKnee))
+    }
+    
+    var waist: MeasurementUnit {
+        return self._waist
+    }
+    
+    var highWaist: MeasurementUnit{
+        return self._highWaist
+    }
+    
+    var hip: MeasurementUnit {
+        return self._hip
+    }
+    
+    var thigh: MeasurementUnit {
+        return self._thigh
+    }
+    
+    var croach: MeasurementUnit {
+        return self._croach
+    }
+    
+    var inseamToKnee: MeasurementUnit {
+        return self._inseamToKnee
+    }
+    
+    var inseamToHeel: MeasurementUnit {
+        return self._inseamToHeel
+    }
+    
+    var waistToAnkle: MeasurementUnit {
+        return self._waistToAnkle
+    }
+    
+    var waistToHeel: MeasurementUnit {
+        return self._waistToHeel
+    }
+    
+    var waistToKnee: MeasurementUnit {
+        return self._waistToKnee
+    }
+    
+}
+
+//MARK: Validation
+extension BottomMeasurement {
+    private static func validateNotNil(
+        waist: MeasurementUnit,
+        highWaist: MeasurementUnit,
+        hip: MeasurementUnit,
+        thigh: MeasurementUnit,
+        croach: MeasurementUnit,
+        inseamToKnee: MeasurementUnit,
+        inseamToHeel: MeasurementUnit,
+        waistToAnkle: MeasurementUnit,
+        waistToHeel: MeasurementUnit,
+        waistToKnee: MeasurementUnit
+    ) -> Bool {
+        let validateWaist = Guard.againstNil(argument: waist)
+        let validateHighWaist = Guard.againstNil(argument: highWaist)
+        let validateHip = Guard.againstNil(argument: hip)
+        let validateThigh = Guard.againstNil(argument: thigh)
+        let validateCroach = Guard.againstNil(argument: croach)
+        let validateInseamToKnee = Guard.againstNil(argument: inseamToKnee)
+        let validateInseamToHeel = Guard.againstNil(argument: inseamToHeel)
+        let validateWaistToAnkle = Guard.againstNil(argument: waistToAnkle)
+        let validateWaistToHeel = Guard.againstNil(argument: waistToHeel)
+        let validateWaistToKnee = Guard.againstNil(argument: waistToKnee)
+        if !validateWaist || !validateHighWaist || !validateHip || !validateThigh || !validateCroach || !validateInseamToKnee || !validateInseamToHeel || !validateWaistToAnkle || !validateWaistToHeel || !validateWaistToKnee {
+            return false
+        }
+        return true
+    }
 }
--- User/User/domian/value_object/measurement/body/FeetMeasurement.swift
+++ User/User/domian/value_object/measurement/body/FeetMeasurement.swift
@@ -7,9 +7,49 @@
 //
 
 import Foundation
+import Core
 
 struct FeetMeasurement : Equatable {
+    private let _ankle: MeasurementUnit
+    private let _ankleToHell: MeasurementUnit
+    private let _heelToToe: MeasurementUnit
     
+    private init(ankle: MeasurementUnit, ankleToHeel: MeasurementUnit, heelToToe: MeasurementUnit) {
+        self._ankle = ankle
+        self._ankleToHell = ankleToHeel
+        self._heelToToe = heelToToe
+    }
+    
+    static func create(with ankle: MeasurementUnit, with ankleToHeel: MeasurementUnit, with heelToToe: MeasurementUnit) -> ResultOption<FeetMeasurement, AppError> {
+        if (!validateForNilValue(ankle: ankle, ankleToHeel: ankleToHeel, heelToToe: heelToToe)) {
+            return .error(AppError.nilValueNotAllowed)
+        }
+        return .ok(FeetMeasurement(ankle: ankle, ankleToHeel: ankleToHeel, heelToToe: heelToToe))
+    }
+    
+    var ankle: MeasurementUnit {
+        return self._ankle
+    }
+    
+    var ankleToHeel: MeasurementUnit {
+        return self._ankleToHell
+    }
+    
+    var heelToToe: MeasurementUnit {
+        return self._heelToToe
+    }
 }
 
+// MARK: Validation
+extension FeetMeasurement {
+    private static func validateForNilValue(ankle: MeasurementUnit, ankleToHeel: MeasurementUnit, heelToToe: MeasurementUnit) -> Bool {
+        let validAnkle = Guard.againstNil(argument: ankle)
+        let validAnkleToHeel = Guard.againstNil(argument: ankleToHeel)
+        let validheelToToe = Guard.againstNil(argument: heelToToe)
+        if !validAnkle || !validAnkleToHeel || !validheelToToe {
+            return false
+        }
+        return true
+    }
+}
 
