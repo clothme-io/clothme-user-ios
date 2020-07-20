7461ebfe2ee8080d3bb35a2c7615ff83e55c89b0
clothme-io <clothme2019@outlook.com>
2020-07-20 15:19:45 -0400

    updated connection

--- User/User/domian/value_object/connection/ConnectionDuration.swift
+++ User/User/domian/value_object/connection/ConnectionDuration.swift
@@ -17,41 +17,13 @@ struct ConnectionDuration : Equatable {
         self._value = connectionDate
     }
     
-    static func create(connectionDate: String) -> ResultOption<ConnectionDuration, AppError> {
-        return validateForNilValue(input: connectionDate)
-        .bind(validateForEmptyValue(input:))
-        .bind(initConnectionDuration(_:))
-    }
-    
-    var value: Int {
-        return self._value
-    }
-    
-
-
-}
-
-extension ConnectionDuration {
-   private static func validateForNilValue (input: String) -> ResultOption<String, AppError> {
-        let validInput = Guard.againstNil(argument: input)
-        if validInput {
-            return .ok(input)
+    static func create(with connectionDate: String) -> ResultOption<ConnectionDuration, AppError> {
+        let validInput = Guard.againstNil(argument: connectionDate)
+        if !validInput {
+            return .error(AppError.negativeValueNotAllowed)
         }
-        return .error(AppError.negativeValueNotAllowed)
-    }
-    
-    private static func validateForEmptyValue (input: String) -> ResultOption<String, AppError> {
-        let validInput = Guard.AgainstEmptyString(argument: input)
-        if validInput {
-            return .ok(input)
-        }
-        return .error(AppError.emptyValueNotAllowed)
-    }
-    
-    private static func initConnectionDuration(_ input: String) -> ResultOption<ConnectionDuration, AppError> {
-        let connectionDateString: String = input
+        let connectionDateString: String = connectionDate
         let dateFormatter = DateFormatter()
-//        dateFormatter.locale = Locale(identifier: "en_US_POSIX") // set locale to reliable US_POSIX
         dateFormatter.dateFormat = "dd-MM-yyyy"
         let startDate = dateFormatter.date(from: connectionDateString)!
         let currentDate = dateFormatter.string(from: Date())
@@ -60,4 +32,9 @@ extension ConnectionDuration {
         let dateDifference = calendar.dateComponents([.day], from: startDate, to: endDate!)
         return .ok(ConnectionDuration(connectionDate: dateDifference.day!))
     }
+    
+    var value: Int {
+        return self._value
+    }
+
 }
