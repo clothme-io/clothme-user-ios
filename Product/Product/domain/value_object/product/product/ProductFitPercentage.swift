d77ffe98682fb202ac661de4a92a3a1c21f7d0b1
clothme-io <clothme2019@outlook.com>
2020-08-08 12:23:03 -0400

    updated product

--- Product/Product/domain/value_object/product/product/ProductFitPercentage.swift
+++ Product/Product/domain/value_object/product/product/ProductFitPercentage.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import Core
+
+public struct ProductFitPercentage {
+    
+    private var _value: Double;
+    
+    private init(value: Double) {
+        self._value = value;
+    }
+    
+    public static func create(with input: Double) -> ResultOption<ProductFitPercentage, AppError> {
+        let validInput = Guard.againstNegative(value: input)
+        if !validInput {
+            return .error(AppError.nilValueNotAllowed)
+        }
+        return .ok(ProductFitPercentage(value: input))
+    }
+    
+    public var value: Double {
+           return self._value
+    }
+    
+}
