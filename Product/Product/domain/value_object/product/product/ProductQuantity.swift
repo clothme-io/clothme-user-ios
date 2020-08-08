c1701eb3786dff3642088e5c4434727a558796c3
clothme-io <clothme2019@outlook.com>
2020-08-08 12:22:27 -0400

    updated product

--- Product/Product/domain/value_object/product/product/ProductQuantity.swift
+++ Product/Product/domain/value_object/product/product/ProductQuantity.swift
@@ -11,4 +11,22 @@ import Core
 
 public struct ProductQuantity: Equatable {
     
+    private var _value: Int;
+    
+    private init(value: Int) {
+        self._value = value;
+    }
+    
+    public static func create(with input: Int) -> ResultOption<ProductQuantity, AppError> {
+        let validInput = Guard.againstNegative(value: Double(Int(input)))
+        if !validInput {
+            return .error(AppError.nilValueNotAllowed)
+        }
+        return .ok(ProductQuantity(value: input))
+    }
+    
+    public var value: Int {
+           return self._value
+    }
+    
 }
--- Product/Product/domain/value_object/product/product/ProductTax.swift
+++ Product/Product/domain/value_object/product/product/ProductTax.swift
@@ -11,4 +11,22 @@ import Core
 
 public struct ProductTax {
     
+    private var _value: Double;
+    
+    private init(value: Double) {
+        self._value = value;
+    }
+    
+    public static func create(with input: Double) -> ResultOption<ProductTax, AppError> {
+        let validInput = Guard.againstNegative(value: input)
+        if !validInput {
+            return .error(AppError.nilValueNotAllowed)
+        }
+        return .ok(ProductTax(value: input))
+    }
+    
+    public var value: Double {
+           return self._value
+    }
+    
 }
