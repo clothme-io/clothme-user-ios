51fc9d020206e2e76a90629d1e85ab065367b07f
clothme-io <clothme2019@outlook.com>
2020-08-15 18:07:41 -0400

    updated company

---
+++ Company/Company/domain/value_object/brand/BrandName.swift
@@ -0,0 +1,9 @@
+//
+//  BrandName.swift
+//  Company
+//
+//  Created by MACPRO on 2020-08-15.
+//  Copyright © 2020 Paul Ikhane. All rights reserved.
+//
+
+import Foundation
---
+++ Company/Company/domain/value_object/location/LocationId.swift
@@ -0,0 +1,9 @@
+//
+//  LocationId.swift
+//  Company
+//
+//  Created by MACPRO on 2020-08-15.
+//  Copyright © 2020 Paul Ikhane. All rights reserved.
+//
+
+import Foundation
