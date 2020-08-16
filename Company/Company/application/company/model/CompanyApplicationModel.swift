18b0aaa1ed9b92b26d571dccea24c745be39335e
clothme-io <clothme2019@outlook.com>
2020-08-16 11:00:59 -0400

    updated company mapper

---
+++ Company/Company/application/company/model/CompanyApplicationModel.swift
@@ -0,0 +1,14 @@
+//
+//  CompanyApplicationModel.swift
+//  Company
+//
+//  Created by MACPRO on 2020-08-16.
+//  Copyright © 2020 Paul Ikhane. All rights reserved.
+//
+
+import Foundation
+import Core
+
+struct CompanyApplicationModel : RequestDataAble {
+    
+}
---
+++ Company/Company/application/location/model/LocationApplicationModel.swift
@@ -0,0 +1,14 @@
+//
+//  LocationApplicationModel.swift
+//  Company
+//
+//  Created by MACPRO on 2020-08-16.
+//  Copyright © 2020 Paul Ikhane. All rights reserved.
+//
+
+import Foundation
+import Core
+
+struct LocationApplicationModel {
+    
+}
