c7682999aaf8f0003dd1544d8f9ec3ebc711dcc9
clothme-io <clothme2019@outlook.com>
2020-08-16 13:40:05 -0400

    location mapper

--- Company/Company/domain/entity/brand/Brand.swift
+++ Company/Company/domain/entity/brand/Brand.swift
@@ -16,4 +16,64 @@ public class Brand: AggregateRoot {
     private let _brandBadge: BrandBadge
     private let _returnPolicy: BrandReturnPolicy
     private let _deliveryPolicy: BrandDeliveryPolicy
+    
+    private init(
+        brandId: BrandId,
+        brandName: BrandName,
+        brandCategory: BrandCategory,
+        brandBadge: BrandBadge,
+        returnPolicy: BrandReturnPolicy,
+        deliveryPolicy: BrandDeliveryPolicy
+    ) {
+        self._brandId = brandId
+        self._brandName = brandName
+        self._brandCategory = brandCategory
+        self._brandBadge = brandBadge
+        self._returnPolicy = returnPolicy
+        self._deliveryPolicy = deliveryPolicy
+        super.init(_id: Guid(value: brandId.value().toIdString()))
+    }
+    
+    static func create(
+        brandId: BrandId,
+        brandName: BrandName,
+        brandCategory: BrandCategory,
+        brandBadge: BrandBadge,
+        returnPolicy: BrandReturnPolicy,
+        deliveryPolicy: BrandDeliveryPolicy
+    ) -> ResultOption<Brand, AppError> {
+        return .ok(Brand(
+            brandId: brandId,
+            brandName: brandName,
+            brandCategory: brandCategory,
+            brandBadge: brandBadge,
+            returnPolicy: returnPolicy,
+            deliveryPolicy: deliveryPolicy)
+        )
+    }
+    
+    func getBrandId() -> BrandId {
+        return self._brandId
+    }
+    
+    func getBrandName() -> BrandName {
+        return self._brandName
+    }
+    
+    func getBrandCategory() -> BrandCategory {
+        return self._brandCategory
+    }
+    
+    func getBrandBadge() -> BrandBadge {
+        return self._brandBadge
+    }
+    
+    func getReturnPolicy() -> BrandReturnPolicy {
+        return self._returnPolicy
+    }
+    
+    func getDeliveryPolicy() -> BrandDeliveryPolicy {
+        return self._deliveryPolicy
+    }
+    
 }
--- Company/Company/domain/entity/company/Company.swift
+++ Company/Company/domain/entity/company/Company.swift
@@ -13,4 +13,40 @@ public class Company: AggregateRoot {
     private let _companyId: CompanyId
     private let _companyName: CompanyName
     private let _companyContact: CompanyContact
+    
+    private init(
+        companyId: CompanyId,
+        companyName: CompanyName,
+        companyContact: CompanyContact
+    ) {
+        self._companyId = companyId
+        self._companyName = companyName
+        self._companyContact = companyContact
+        super.init(_id: Guid(value: companyId.value().toIdString()))
+    }
+    
+    static func create(
+        companyId: CompanyId,
+        companyName: CompanyName,
+        companyContact: CompanyContact
+    ) -> ResultOption<Company, AppError> {
+        return .ok(Company(
+            companyId: companyId,
+            companyName: companyName,
+            companyContact: companyContact)
+        )
+    }
+    
+    func getCompanyId() -> CompanyId {
+        return self._companyId
+    }
+    
+    func getCompanyName() -> CompanyName {
+        return self._companyName
+    }
+    
+    func getCompanyContact() -> CompanyContact {
+        return self._companyContact
+    }
+    
 }
